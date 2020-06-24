cc74e45ba0e4a9ed805940549ec34fa66688fffb
LT <369259353@qq.com>
2020-06-24 10:25:31 +0800

    ROG

--- src/com/im/utils/chat/ServerManager.java
+++ src/com/im/utils/chat/ServerManager.java
@@ -57,26 +57,26 @@ public class ServerManager {
     }
    
     public void nettyServer() {
-		EventLoopGroup bossGroup = new NioEventLoopGroup(); // (1)
+		EventLoopGroup bossGroup = new NioEventLoopGroup(); 
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
-            ServerBootstrap b = new ServerBootstrap(); // (2)
+            ServerBootstrap b = new ServerBootstrap(); 
             if(nettype.equals("socket")){
            	 	b.group(bossGroup, workerGroup)
-                .channel(NioServerSocketChannel.class) // (3)
-                .childHandler(new SocketServerInitializer())  //(4)
-                .option(ChannelOption.SO_BACKLOG, 128)          // (5)
-                .childOption(ChannelOption.SO_KEEPALIVE, true); // (6)
+                .channel(NioServerSocketChannel.class) 
+                .childHandler(new SocketServerInitializer()) 
+                .option(ChannelOption.SO_BACKLOG, 128)         
+                .childOption(ChannelOption.SO_KEEPALIVE, true);
             }
             else if(nettype.equals("websocket")){
              
            	 	b.group(bossGroup, workerGroup)
-                .channel(NioServerSocketChannel.class) // (3)
-                .childHandler(new WebsocketServerInitializer())  //(4)
-                .option(ChannelOption.SO_BACKLOG, 128)          // (5)
-                .childOption(ChannelOption.SO_KEEPALIVE, true); // (6)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new WebsocketServerInitializer()) 
+                .option(ChannelOption.SO_BACKLOG, 128)         
+                .childOption(ChannelOption.SO_KEEPALIVE, true); 
             }
-            if(cacheType==null){
+            if(cacheType==null||cacheType.equals("")){
             	 logger.info("用户同步方式:"+"单服务不需要同步！");
             }
             else{
@@ -85,9 +85,9 @@ public class ServerManager {
             logger.info("服务类型："+nettype);
             logger.info("启动IMServer");
             
-    		//参数 
+    		
             int port=Integer.parseInt(socketPort);
-            ChannelFuture f = b.bind(port).sync(); // (7)
+            ChannelFuture f = b.bind(port).sync();
             f.channel().closeFuture().sync();
         }
         catch (Exception e) {
@@ -102,5 +102,6 @@ public class ServerManager {
     }
 	
 	public static void main(String[] args){
+		
 	}
 } 
